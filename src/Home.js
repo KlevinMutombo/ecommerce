9337b12c95ea33478528bef7c934d9b4ed168ac1
KlevinMutombo <klevin.mutombo@gmail.com>
2025-07-03 16:58:21 -0400

    image fixes

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,14 @@
 // src/Home.js
 import React from 'react';
 import heroShoe from './vapormax.jpeg';  // path to your image
-import './App.css';
+import './Home.css';
 
 export default function Home() {
   return (
     <div className="home">
       <section className="home-hero">
         <div className="text">
-          <h1>Find your dream sneakers</h1>
+          <h1>Find your dream sneakers.</h1>
           <p>Discover our exclusive collection of top-quality kicks.</p>
           <button onClick={() => window.location.href = '/products'}>
             Explore Our Products
@@ -22,15 +22,15 @@ export default function Home() {
       <section className="features">
         <div className="feature-card">
           <img src="/Imagesss/payment.avif" alt="Secure Payment" />
-          <p>Secure Payment</p>
+          <p>Secure Payment.</p>
         </div>
         <div className="feature-card">
           <img src="/Imagesss/support.png" alt="24/7 Support" />
-          <p>24/7 Support</p>
+          <p>24/7 Support.</p>
         </div>
         <div className="feature-card">
           <img src="/Imagesss/delivery.png" alt="Fast Delivery" />
-          <p>Fast Delivery</p>
+          <p>Fast Delivery.</p>
         </div>
       </section>
     </div>
